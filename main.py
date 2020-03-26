cc2dc482bfa1aaa1bb95d4892320d025c55ce3bc
Vakhtang Dolidze <58568435+Fedaykin1980@users.noreply.github.com>
2020-03-26 15:02:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from application.db.people import get_employees
 from application.sallary import calculate_salary
+import datetime
+
 
 if __name__ == '__main__':
     calculate_salary()
     get_employees()
+    time = datetime.datetime.now()
+    print(time.strftime("%d-%m-%Y"))
 
